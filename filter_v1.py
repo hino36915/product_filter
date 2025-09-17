67eaff3a654260e8c54fd3d328f11c4b21a9b97d
hino36915 <69636213+hino36915@users.noreply.github.com>
2025-09-17 10:10:12 +0800

    修正多餘的欄位
    (cherry picked from commit 77183a570be76acb39ffd324a453fde91ee41169)

--- filter_v1.py
+++ filter_v1.py
@@ -10,16 +10,60 @@ all_files = [
     "detail_sku_attr-20250902.csv",
     "detail_sku_image-sampled-20250902.csv",
 ]
+
+file_configs = {
+    "prod_main-sampled-20250902.csv": {
+        "usecols": ["prod_id", "subject"]
+    },
+    "prod_detail-sampled-20250902.csv":{
+        "usecols": ["prod_id", "description"]
+    },
+    "detail_image-sampled-20250902.csv": {
+        "usecols": ["prod_id", "image_url"]
+    },
+    "detail_sku-sampled-20250902.csv": {
+        "usecols": ["prod_id","vendor_price", "price", "sku_id"]
+    },
+    "prod_category-sampled-20250902.csv": {
+        "usecols": ["prod_id", "prod_level_path"]
+    },
+      "detail_sku_attr-20250902.csv": {
+        "usecols": ["prod_id", "attr_name","value","sku_id"]
+    },
+    "detail_sku_image-sampled-20250902.csv": {
+        "usecols": ["prod_id",  "image_url","sku_id"]
+    }
+}
 dataframes = {}
+
+
 try:
-    merge_df = pd.read_csv(all_files[0])
 
-    for file_path in all_files[1:]:
-        temp_df = pd.read_csv(file_path)
-        merge_df = pd.merge(merge_df, temp_df, on="prod_id", how="left")
+    for file_path in all_files:
+        if file_path in file_configs:
+            
+            print(f"正在讀取: {file_path}...")
+            # 根據設定檔，讀取該檔案特定的欄位
+            config = file_configs[file_path]
+            df = pd.read_csv(file_path, usecols=config["usecols"])
+            dataframes[file_path] = df
+        
+        else:
+              print(f"警告：檔案 {file_path} 沒有對應的設定，已跳過。")        
 
-    print("\n 所有檔案合併完成！")
 
 
+        merge_df =dataframes["prod_main-sampled-20250902.csv"]
+    
+    for file_name in all_files[1:]:
+        if file_name in all_files[1:]:
+            print(f"正在合併 : {file_name}")
+            merge_df = pd.merge(merge_df,
+                                  dataframes[file_name],
+                                  on="prod_id",
+                                  how="left"
+                                  )
+     
+        
 except Exception as e:
-    print(f"\n exception 題：{e}。")
+    print(f"\n exception ：{e}。")
